1ca3cf875080cca0908309fdd7f32ba23376107c
Smiggy <70798673+Smigg-y@users.noreply.github.com>
2024-11-28 02:50:22 +0000

    rewrite api file

--- src/main/ipc/api.ts
+++ src/main/ipc/api.ts
@@ -1,27 +1,4 @@
-import axios, { AxiosError } from 'axios'
-
-async function API<T>(url: string): Promise<APIResponse<T | null>> {
-  try {
-    const { data, status } = await axios.get<T>(url)
-    return { status, ok: status === 200, data: status === 200 ? data : null }
-  } catch (e) {
-    const error = e as AxiosError
-    const status = error.response?.status || -1
-    return { status, ok: false, data: null }
-  }
-}
-
-function APIHandler<T>(
-  ident: APIEndpoint,
-  url: string
-): [string, (event: any, args: any[]) => Promise<APIResponse<T | null>>] {
-  return [
-    ident,
-    async (_, args) => {
-      return await API<T>(url.format(...args))
-    }
-  ]
-}
+import axios, { AxiosError, AxiosResponse, Method } from 'axios'
 
 String.prototype.format = function (): string {
   const args = arguments
@@ -30,20 +7,84 @@ String.prototype.format = function (): string {
   })
 }
 
-const API_URL = 'https://chivalry2stats.com:8443'
+type APIResponse<T> = {
+  status: number
+  ok: boolean
+  data: T | null
+}
 
-export function InitializeAPIHandlers(f: Function) {
-  f(...APIHandler<PlayFabDetails>('fetch_playfab_data', `${API_URL}/players/{0}`))
-  f(
-    ...APIHandler<PlayerDetails>(
-      'fetch_player_data',
-      `${API_URL}/players/fetch-all-leaderboards-around-player/{0}?distinctId=undefined`
-    )
-  )
-  f(
-    ...APIHandler<NameLookup>(
-      'name_lookup',
-      `${API_URL}/players/search?searchTerm={0}&page={1}&pageSize={2}&distinctId=undefined`
-    )
-  )
+class APICommandBuilder<T> {
+  private method: Method = 'GET'
+  private url: string = ''
+  private bodyParams: Record<string, any> = {}
+
+  public setMethod(method: Method) {
+    this.method = method
+    return this
+  }
+
+  public setURL(url: string) {
+    this.url = url
+    return this
+  }
+
+  public setBodyParams(bodyParams: Record<string, any>) {
+    this.bodyParams = bodyParams
+    return this
+  }
+
+  private response(res: AxiosResponse<T>) {
+    return {
+      status: res.status,
+      ok: res.status === 200,
+      data: res.status === 200 ? res.data : null
+    }
+  }
+
+  private error(e: unknown) {
+    const error = e as AxiosError
+    const status = error.response?.status || -1
+    return { status, ok: false, data: null }
+  }
+
+  public async execute(args: any[]): Promise<APIResponse<T>> {
+    try {
+      return this.response(
+        await axios({
+          method: this.method,
+          url: this.url.format(...args),
+          data: this.bodyParams
+        })
+      )
+    } catch (e) {
+      return this.error(e)
+    }
+  }
+}
+
+const API_URL = 'https://chivalry2stats.com:8443'
+const Commands = {
+  fetch_playfab_data: (_, args) =>
+    new APICommandBuilder<PlayFabDetails>()
+      .setMethod('GET')
+      .setURL(`${API_URL}/api/player/findByPlayFabId/{0}`)
+      .execute(args),
+
+  fetch_player_data: (_, args) =>
+    new APICommandBuilder<PlayerDetails>()
+      .setMethod('GET')
+      .setURL(`${API_URL}/api/player/getLeaderboardAroundPlayer/{0}?distinctId=undefined`)
+      .execute(args),
+  name_lookup: (_, args) =>
+    new APICommandBuilder<NameLookup>()
+      .setMethod('POST')
+      .setURL(`${API_URL}/api/player/usernameSearch/{0}`)
+      .setBodyParams({ page: args[1], pageSize: 10, distinctId: 'undefined' })
+      .execute(args)
+}
+
+export function InitializeAPIHandlers(handle: Function) {
+  for (const [channel, listener] of Object.entries(Commands)) {
+    handle(channel, listener)
+  }
 }
